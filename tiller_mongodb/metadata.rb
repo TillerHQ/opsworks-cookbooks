abc78749de80912a7293334f970ed32a2b36efcb
Tim Johns <timjohns@yahoo.com>
2016-01-04 16:31:14 -0800

    Added mongodb configuration

---
+++ tiller_mongodb/metadata.rb
@@ -0,0 +1,8 @@
+name        "mongodb-big-tree-service"
+description 'Cookbook for MongoDB Berkshelf based install'
+maintainer  "Big Tree Money"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
+
